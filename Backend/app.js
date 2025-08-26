57d1c6233a957a2ec82630e11f825f1e0b37ec10
Sonu <sonu108rp@gmail.com>
2025-08-27 02:27:36 +0530

    login signup added

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,18 @@ const port = 8080;
 require("dotenv").config();
 const mongo_url = process.env.MONGO_URL;
 const userModel = require("./Models/userModel");
+const userRoutes = require("./Routes/user.routes");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
 app.get("/",async(req,res)=>{
-    const op =await userModel.find();
-    console.log("db: ",op);
     res.json({"message":"server started"});
 })
+
+app.use("/user",userRoutes);
+
+
 const main = async()=>{
     console.log("connecting to database");
     await mongoose.connect(mongo_url)
---
+++ Backend/readME.md
@@ -0,0 +1,22 @@
+# Backend File Instructions
+**1. Make sure you are in /Backend Directory**
+**2. Install dependency**
+```bash
+npm install
+```
+
+**3. Set env**
+
+- MONGO_URL
+- JWT_SECRET
+
+ **4. Start the server**
+
+```bash
+npm run dev
+```
+
+## Backend routes detail
+- "/user/login" - login route
+- "/user/signup" - signup route
+- "/user" - get all users
